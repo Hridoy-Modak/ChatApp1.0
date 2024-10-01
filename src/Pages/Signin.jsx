f582b8124adadf168f64ca4d6778c3dc3e13c128
Hridoy Chandra Modak <hridoychandramodak@gmail.com>
2024-10-01 22:04:56 +0600

    signin with google function added

--- src/Pages/Signin.jsx
+++ src/Pages/Signin.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import Signinimg from "../assets/images/signin.png";
-import { FaUserCircle } from "react-icons/fa";
+import { FaRegEye, FaUserCircle } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook } from "react-icons/fa6";
 import { Link } from "react-router-dom";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
+import { IoIosEyeOff } from "react-icons/io";
 
 const Signin = () => {
-  // const auth = getAuth();
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+  const [passShow, setPassShow] = useState(false);
   let [email, setEmail] = useState("");
   let [password, setPassword] = useState("");
   let [emailerr, setEmailerr] = useState("");
@@ -19,8 +23,8 @@ const Signin = () => {
   let handlepassword = (e) => {
     setPassword(e.target.value);
   };
+
   let handlesubumit = () => {
-    console.log(email, password);
     if (!email) {
       setEmailerr("Email is required");
     } else if (!/[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}/.test(email)) {
@@ -31,8 +35,49 @@ const Signin = () => {
       setPassworderr("Password is required");
       valid = false;
     }
-    console.log(passworderr);
+    if (email && password){
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        if(error.code.includes("auth/invalid-credential")){
+          setEmailerr("invalid-credential")
+          
+        }
+      });
+    }
+
   };
+
+  let hanglegoogle=()=>{
+    signInWithPopup(auth, provider)
+  .then((result) => {
+    // // This gives you a Google Access Token. You can use it to access the Google API.
+    // const credential = GoogleAuthProvider.credentialFromResult(result);
+    // const token = credential.accessToken;
+    // // The signed-in user info.
+    // const user = result.user;
+    // // IdP data available using getAdditionalUserInfo(result)
+    // // ...
+    console.log(result)
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+    console.log(errorCode)
+  });
+  }
+
 
   return (
     <div className="flex relative bg-[rgb(236,222,228)] bg-[radial-gradient(circle,_rgba(236,222,228,1)_0%,_rgba(167,225,233,1)_24%,_rgba(245,254,249,1)_67%,_rgba(115,179,255,1)_100%)]">
@@ -70,12 +115,23 @@ const Signin = () => {
             <div className="relative">
               <input
                 onChange={handlepassword}
+                type={passShow ? "text" : "password"}
                 className={`border ${
                   passworderr ? "border-red-500" : ""
                 } placeholder:text-[13px] px-[20px] py-3 rounded-lg placeholder:opacity-40 w-full shadow-md `}
-                type="password"
                 placeholder="Enter your password"
               />
+              {passShow ? (
+                <FaRegEye
+                  onClick={() => setPassShow(false)}
+                  className="absolute top-[50%] translate-y-[-50%] right-3 text-[20px]"
+                />
+              ) : (
+                <IoIosEyeOff
+                  onClick={() => setPassShow(true)}
+                  className="absolute top-[50%] translate-y-[-50%] right-3 text-[20px]"
+                />
+              )}
               {passworderr && (
                 <p className="text-red-600 text-[10px] ml-1">{passworderr}</p>
               )}
@@ -96,7 +152,9 @@ const Signin = () => {
           <p className="text-[12px] mt-3">Forgot Password ?</p>
           <p className="text-[15px] mt-3">OR</p>
           <div className="social_login flex gap-3 mt-2 justify-center">
-            <div className="bg-white h-[60px] items-center gap-2 px-1 w-[150px] rounded-lg flex cursor-pointer justify-center">
+            <div
+            onClick={hanglegoogle}
+             className="bg-white h-[60px] items-center gap-2 px-1 w-[150px] rounded-lg flex cursor-pointer justify-center">
               <FcGoogle className="text-[50px] w-[40%] px-1 border-r-2" />
               <p className=" text-[15px] text-center mr-1">Login with Google</p>
             </div>
